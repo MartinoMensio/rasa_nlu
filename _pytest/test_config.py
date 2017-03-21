a4ecd1986da33df98070f3bc92c893c360189054
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2017-03-21 16:59:11 +0800

    dropped default config test

--- _pytest/test_config.py
+++ _pytest/test_config.py
@@ -2,31 +2,6 @@ import tempfile
 
 from rasa_nlu.config import RasaNLUConfig
 import json
-import os
-
-
-with open("config_defaults.json", "r") as f:
-    defaults = json.load(f)
-    # Special treatment for these two, as they are absolute directories
-    defaults["path"] = os.path.join(os.getcwd(), defaults["path"])
-    defaults["response_log"] = os.path.join(os.getcwd(), defaults["response_log"])
-
-
-def test_default_config():
-    final_config = RasaNLUConfig()
-    assert dict(final_config.items()) == defaults
-
-
-def test_blank_config():
-    file_config = {}
-    cmdline_args = {}
-    env_vars = {}
-    with tempfile.NamedTemporaryFile(suffix="_tmp_config_file.json") as f:
-        f.write(json.dumps(file_config))
-        f.flush()
-        final_config = RasaNLUConfig(f.name, env_vars, cmdline_args)
-        assert dict(final_config.items()) == defaults
-
 
 def test_file_config_unchanged():
     file_config = {"path": "/path/to/dir"}
